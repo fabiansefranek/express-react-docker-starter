e3f5020787d809c6a35a805db8614cd712359aad
Fabian <fabiansefranek@gmail.com>
2022-12-26 22:32:42 +0100

    ✨ Add server connection test to App component

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,7 +1,18 @@
-import React from "react";
+import { useState, useEffect } from "react";
+
+const API_URL = `http://localhost:${process.env.REACT_APP_API_PORT}/api`;
 
 function App() {
-    return <></>;
+    const [message, setMessage] = useState<string>("");
+
+    useEffect(() => {
+        fetch(`${API_URL}/hello`)
+            .then((res) => res.json())
+            .then((data) => setMessage(data.message))
+            .catch((err) => setMessage("Error. Could not connect to server."));
+    }, []);
+
+    return <>{message}</>;
 }
 
 export default App;
